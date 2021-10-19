da42c6938c645dcc7c1d233e62e8e9986c777db6
SergeyLipatnikov <lipat995@gmail.com>
2021-10-20 00:58:06 +0300

    upd LinkedList task

--- lLinkedList.py
+++ lLinkedList.py
@@ -41,37 +41,39 @@ class LinkedList:
         return Found_nodes
 
     def delete(self, val, all):
+        prevNode = None
         node = self.head
-        if not node:
-            return
-        elif node.value == val:
-            if node.next is None:
-                self.clean()
-                return
-            node.value = node.next.value
-            node.next = node.next.next 
+        if node is not None:
+            if node.value == val:
+                self.head = node.next
+                if not all:
+                    return
         if all:
             while node is not None:
-                if (node.next != None) and (node.next.value == val):
-                    node.next = node.next.next
-                    while node is not None:
-                        if (node.next != None) and (node.next.value == val):
-                            node.next = node.next.next
-                        else:
-                            if node.next is None:
-                                self.tail.next = node
-                                self.tail = node
-                            break
-                node = node.next
+                _del = 0
+                if node.value == val:
+                    if prevNode is not None:
+                        prevNode.next = node.next
+                        _del = 1
+                    else:
+                        self.head = node.next
+                        _del = 1
+                if _del == 0:
+                    prevNode = node
+                    node = node.next
+                else:
+                    node = node.next
         else:
             while node is not None:
-                if (node.next != None) and node.next.value == val:
-                    node.next = node.next.next
-                    if node.next is None:
-                        self.tail.next = node
-                        self.tail = node
+                if node.value == val:
                     break
+                lastnode = node
                 node = node.next
+        if node == None:
+            self.tail = prevNode
+            return
+        lastnode.next = node.next
+        node = None
 
     def clean(self):
         self.head = None
@@ -94,39 +96,8 @@ class LinkedList:
         else:
             node = self.find(afterNode)
             if node.next is None:
-                self.tail.next = Node(newNode)
-                self.tail = Node(newNode)
+                self.add_in_tail(Node(newNode))
             else:
                 second_node = Node(newNode)
                 second_node.next = node.next
                 node.next = second_node
-
-n1 = Node(12)
-# n2 = Node(55)
-s_list = LinkedList()
-s_list.add_in_tail(n1)
-# s_list.add_in_tail(n2)
-s_list.add_in_tail(Node(12))
-s_list.add_in_tail(Node(128))
-s_list.add_in_tail(Node(12))
-# s_list.add_in_tail(Node(128))
-s_list.add_in_tail(Node(12))
-# s_list.add_in_tail(Node(128))
-# s_list.add_in_tail(Node(12))
-# s_list.add_in_tail(Node(128))
-# s_list.add_in_tail(Node(77))
-# s_list.add_in_tail(Node(128))
-# s_list.add_in_tail(Node(128))
-# s_list.add_in_tail(Node(128))
-# s_list.print_all_nodes()
-# print(s_list.len())
-# print(s_list.find_all(128))
-# s_list.insert(55,36)
-s_list.delete(12,True)
-s_list.print_all_nodes()
-# print(s_list.head.value,s_list.head.next.value, s_list.tail.value, s_list.tail.next)
-# # s_list.delete(12,True)
-# # s_list.print_all_nodes()
-# # s_list.clean()
-# # s_list.print_all_nodes()
-# # print(s_list.head, s_list.tail)
